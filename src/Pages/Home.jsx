5c5d85fe108031226b9f280e6efe54b1a7bfa000
Amityadavk <amityadavpbh72@gmail.com>
2023-05-02 19:04:47 +0530

    Resolve:Qty is working fine

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,162 +1,202 @@
-import React from "react";
 import { useEffect, useState } from "react";
 import Products from "../Components/Product";
 
-import "./home.css"
+import "./home.css";
 import Navbar from "../Components/Navbar";
 import { Cart } from "../context/Context";
 import { useContext } from "react";
 
-
-
 function Home() {
-    // const [product, setProduct] = useState([]);
-    const [searchinput, setSearchInput] = useState("");
-    const [lastSearch, setLastSearch] = useState("")
-    const [loader, setLoader] = useState(false);
-    const [cartItem, setCartItem] = useState(0);
-    // const [productCheck, setProductCheck] = useState(true);
+  // const [product, setProduct] = useState([]);
+  const [searchinput, setSearchInput] = useState("");
+  const [lastSearch, setLastSearch] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [cartItem, setCartItem] = useState(0);
+  // const [productCheck, setProductCheck] = useState(true);
 
-    const cartTotal = useContext(Cart);
+  const cartTotal = useContext(Cart);
 
-    function search(value) {
-        console.log(value);
-        setLoader(true)
-        fetch(`https://dummyjson.com/products/search?q=${value}`)
-            .then((res) => res.json())
-            .then((data) => {
-                // setProduct(data.products);
-                cartTotal.dispatch({
-                    type: "total product",
-                    payload: data.products
-                })
+  function search(value) {
+    console.log(value);
+    setLoader(true);
+    fetch(`https://dummyjson.com/products/search?q=${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // setProduct(data.products);
+        cartTotal.dispatch({
+          type: "total product",
+          payload: data.products,
+        });
 
-                setLoader(false)
-                console.log(data.products);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        setLoader(false);
+        console.log(data.products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
+    // let tempobject = { textinput: value };
+    // console.log(tempobject);
+    // setProduct(tempobject)
+    setLastSearch(searchinput);
+    setSearchInput("");
+  }
 
-        // let tempobject = { textinput: value };
-        // console.log(tempobject);
-        // setProduct(tempobject)
-        setLastSearch(searchinput)
-        setSearchInput("");
-
-
+  function pressEnter(e) {
+    if (e.key === "Enter") {
+      // console.log("Enter Key Pressed");
+      searchinput.length > 0 && search(searchinput);
     }
+  }
 
-    function pressEnter(e) {
-        if (e.key === 'Enter') {
-            // console.log("Enter Key Pressed");
-            searchinput.length > 0 && search(searchinput)
-        }
+  function productApi() {
+    setLoader(true);
+    fetch("https://dummyjson.com/products?limit=100&page=0")
+      .then((res) => res.json())
+      .then((data) => {
+        // setProduct(data.products);
+        // setProductCheck(true)
+        // totalProduct()
+        // let arr = data.products;
+        data.products.map((item) => {
+          item.qty = 1;
+        });
+        console.log(data.products);
+        cartTotal.dispatch({
+          type: "total product",
+          payload: data.products,
+        });
+        // console.log(data.products)
+
+        setLoader(false);
+        // console.log(data.products);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+      });
+  }
+
+  useEffect(() => {
+    if (cartTotal.state.totalProduct.length === 0) {
+      productApi();
     }
+  }, []);
 
+  // console.log(product);
 
-    function productApi() {
-        setLoader(true)
-        fetch("https://dummyjson.com/products?limit=100&page=0")
-            .then((res) => res.json())
-            .then((data) => {
-                // setProduct(data.products);
-                // setProductCheck(true)
-                // totalProduct()
-                let arr = data.products;
-                for (let i = 0; i < data.products.length; i++) {
-                    arr[i].qty = 1
-                }
-                console.log(arr);
-                cartTotal.dispatch({
-                    type: "total product",
-                    payload: arr
-                })
-                // console.log(data.products)
+  // function totalProduct() {
+  //     // cartTotal.dispatch({
+  //     //     type: "total product",
+  //     //     payload: product
+  //     // })
+  //     console.log(cartTotal);
+  // }
+  // useEffect(() => {
+  //     let arr = cartTotal.state.totalProduct;
+  //     for (let i = 0; i < cartTotal.state.totalProduct.length; i++) {
+  //         arr[i].qty = 1;
+  //     }
+  //     console.log(arr);
+  // }, [totalProduct])
+  // console.log(totalProduct);
 
+  useEffect(() => {
+    setCartItem(cartTotal.state.basket.length);
+  }, [cartTotal.state.basket]);
 
-                setLoader(false)
-                // console.log(data.products);
-            })
-            .catch((err) => {
-                console.log(err);
-                setLoader(false)
+  // console.log(cartTotal);
 
-            });
-    }
+  return (
+    <>
+      {/* <button onClick={totalProduct}>total product</button> */}
+      <Navbar
+        pressEnter={pressEnter}
+        searchinput={searchinput}
+        setSearchInput={setSearchInput}
+        search={search}
+        cartvalue={cartItem}
+      />
+      
 
-    useEffect(() => {
-
-        if (cartTotal.state.totalProduct.length === 0) {
-            productApi()
-        }
-    }, []);
-
-    // console.log(product);
-
-    function totalProduct() {
-        // cartTotal.dispatch({
-        //     type: "total product",
-        //     payload: product
-        // })
-        console.log(cartTotal);
-    }
-    // useEffect(() => {
-    //     let arr = cartTotal.state.totalProduct;
-    //     for (let i = 0; i < cartTotal.state.totalProduct.length; i++) {
-    //         arr[i].qty = 1;
-    //     }
-    //     console.log(arr);
-    // }, [totalProduct])
-    // console.log(totalProduct);
-
-
-    useEffect(() => {
-        setCartItem(cartTotal.state.basket.length);
-    }, [cartTotal.state.basket])
-
-
-
-
-    // console.log(cartTotal);
-
-
-
-
-
-    return <>
-        <button onClick={totalProduct}>total product</button>
-
-        <Navbar
-            pressEnter={pressEnter}
-            searchinput={searchinput}
-            setSearchInput={setSearchInput}
-            search={search}
-            cartvalue={cartItem}
-        />
-
-        {!loader && <div className="total-card">
-            {cartTotal.state.totalProduct.map((item, index) => (
-                <Products
-                    key={index}
-                    image={item.thumbnail}
-                    category={item.category}
-                    price={item.price}
-                    rating={item.rating}
-                    title={item.title}
-                    description={item.description}
-                    id={item.id}
-                    stock={item.stock}
-                    qty={item.qty}
-                />
-            ))}
-        </div>}
-        {loader && <h1 className="loader">Loading...</h1>}
-        {cartTotal.state.totalProduct.length === 0 && loader === false && <h1>No Data Found {lastSearch} </h1>}
-
+      {!loader && (
+        <div className="total-card">
+            <div className="sort-filter-background-div">
+        <div className="sort-filter">
+          <select
+          // onChange={(e) => {
+          //   setSort(e.target.value);
+          // }}
+          // name="price"
+          // id="price"
+          >
+            <option value="">Sort</option>
+            <option disabled></option>
+            <option value="htl">Price: High to Low</option>
+            <option disabled></option>
+            <option value="lth">Price: Low to High</option>
+          </select>
+          <select
+          // onChange={(e) => {
+          //   setCategory(e.target.value);
+          // }}
+          // name="beds"
+          // id="beds"
+          >
+            <option value="">Category</option>
+            <option value="#" disabled></option>
+            <option value="Stool">smartphones</option>
+            <option value="#" disabled></option>
+            <option value="Bed">laptops</option>
+            <option value="#" disabled></option>
+            <option value="Chaise">lighting</option>
+            <option value="#" disabled></option>
+            <option value="LED">home-decoration</option>
+            <option value="#" disabled></option>
+            <option value="Chair">mens-shoes</option>
+            <option value="#" disabled></option>
+            <option value="Wardrobe">sunglasses</option>
+          </select>
+          <select
+          // onChange={(e) => {
+          //   setPrice(e.target.value);
+          // }}
+          // name="price-range"
+          // id="price-range"
+          >
+            <option value="0">Price</option>
+            <option value="#" disabled></option>
+            <option value="4999">₹0 to 4999</option>
+            <option value="#" disabled></option>
+            <option value="9999">₹5,000 to 9999</option>
+            <option value="#" disabled></option>
+            <option value="14999">₹10,000 to 14999</option>
+            <option value="#" disabled></option>
+            <option value="15000">₹15,000 and above</option>
+          </select>
+        </div>
+      </div>
+          {cartTotal.state.totalProduct.map((item, index) => (
+            <Products
+              key={index}
+              image={item.thumbnail}
+              category={item.category}
+              price={item.price}
+              rating={item.rating}
+              title={item.title}
+              description={item.description}
+              id={item.id}
+              stock={item.stock}
+              qty={item.qty}
+            />
+          ))}
+        </div>
+      )}
+      {loader && <h1 className="loader">Loading...</h1>}
+      {cartTotal.state.totalProduct.length === 0 && loader === false && (
+        <h1>No Data Found {lastSearch} </h1>
+      )}
     </>
-
-
+  );
 }
 export default Home;
--- src/context/Reducers.jsx
+++ src/context/Reducers.jsx
@@ -1,129 +1,136 @@
-
 import Context from "./Context";
 const cartReducer = (state, action) => {
-    console.log("action", state);
-    // if(action.type==="add to cart") {
+  console.log("action", state);
+  // if(action.type==="add to cart") {
 
-    //     return {...state, basket: [...state.basket, {...action.payload}]}
-    //     // return [...state[0].basket,action[0].payload]
-    // }
+  //     return {...state, basket: [...state.basket, {...action.payload}]}
+  //     // return [...state[0].basket,action[0].payload]
+  // }
 
-    // if (action.type === "increase qty") {
-    //     let updatedQty = state.basket.map((item) => {
-    //         if (item.id === action.payload.id) {
-    //             console.log(item);
-    //             let increQty = item.qty + 1;
+  // if (action.type === "increase qty") {
+  //     let updatedQty = state.basket.map((item) => {
+  //         if (item.id === action.payload.id) {
+  //             console.log(item);
+  //             let increQty = item.qty + 1;
 
-    //             return { ...item, qty: increQty }
-    //         }else{
-    //             return item;
-    //         }
-    //     });
-    //     return {...state, basket:updatedQty}
+  //             return { ...item, qty: increQty }
+  //         }else{
+  //             return item;
+  //         }
+  //     });
+  //     return {...state, basket:updatedQty}
 
-    // }
+  // }
 
-    switch (action.type) {
-        // console.log(action.type)
+  switch (action.type) {
+    // console.log(action.type)
 
+    case "add to cart":
+      return { ...state, basket: [...state.basket, { ...action.payload }] };
 
-        case "add to cart":
-            return { ...state, basket: [...state.basket, { ...action.payload }] }
+    case "remove from cart":
+      const updatedCart = state.basket.filter(
+        (item) => item.id !== action.payload.id
+      );
+      console.log("updatedcart", updatedCart);
 
-        case "remove from cart":
-            const updatedCart = state.basket.filter((item) => (
+      return { ...state, basket: updatedCart };
 
-                item.id !== action.payload.id
-            ));
-            console.log("updatedcart", updatedCart);
+    case "increase qty":
+      const updatedCartQty = state.basket.map((item) => {
+        console.log(item);
+        if (item.id === action.payload.id) {
+          let increQty = item.qty + 1;
+          if (item.qty === item.stock) {
+            return { ...item, qty: item.stock };
+          }
+          return { ...item, qty: increQty };
+        } else {
+          return item;
+        }
+      });
+      const updatedQty = state.totalProduct.map((item) => {
+        console.log(item);
+        if (item.id === action.payload.id) {
+          let increQty = item.qty + 1;
+          if (item.qty ===item.stock) {
+                return { ...item, qty: item.stock }
+            }
+          return { ...item, qty: increQty };
+        } else {
+          return item;
+        }
+      });
 
-            return { ...state, basket: updatedCart };
+      return { ...state, basket: updatedCartQty, totalProduct: updatedQty };
 
-        case "increase qty":
-            const updatedQty = state.totalProduct.map((item) => {
-console.log(item);
-                if (item.id === action.payload.id) {
-                    let increQty = item.qty + 1;
-                    // if(item.id === action.payload.id){
-                    // let newPrice = item.price *item.qty;
-                    //     return {...item, price:newPrice}
-                    // }
-                    // if (item.qty ===item.stock) {
-                    //     return { ...item, qty: item.stock }
-                    // }
-                    return { ...item, qty: increQty }
+      
 
-                }
+    case "decrease qty":
+      const updatedDecreaseCartQty = state.basket.map((item) => {
+        if (item.id === action.payload.id) {
+          const decreQty = item.qty - 1;
+          if (item.qty <= 1) {
+            return { ...item, qty: 1 };
+          }
+          return { ...item, qty: decreQty };
+        } else {
+          return item;
+        }
+      });
+      const updatedDecreaseQty = state.totalProduct.map((item) => {
+        if (item.id === action.payload.id) {
+          const decreQty = item.qty - 1;
+          if (item.qty <= 1) {
+            return { ...item, qty: 1 };
+          }
+          return { ...item, qty: decreQty };
+        } else {
+          return item;
+        }
+      });
 
-                else {
-                    return item;
-                }
+      return { ...state, basket: updatedDecreaseCartQty, totalProduct: updatedDecreaseQty };
 
-            });
-            return { ...state, totalProduct: updatedQty }
+    case "cart total price":
+      const totalPrice = state.basket.reduce((initialVal, item) => {
+        // let price = item;
 
-        case "decrease qty":
-            const updatedDecreaseQty = state.basket.map((item) => {
+        initialVal = initialVal + item.price * item.qty;
+        console.log(initialVal);
+        return initialVal;
+      }, 0);
+      return { ...state, total_price: totalPrice };
 
-                if (item.id === action.payload.id) {
-                    const decreQty = item.qty - 1;
-                    if (item.qty <= 1) {
-                        return { ...item, qty: 1 }
-                    }
-                   
+    case "total product":
+      // return { ...state, basket: [...state.basket, { ...action.payload }] }
+      // let temp = [1, 2, 3, 4, 5, 6];
+      return { ...state, totalProduct: [...action.payload] };
 
-                    return { ...item, qty: decreQty }
-                } else {
-                    return item;
-                }
+    default:
+      return state;
+  }
 
-            });
-            return { ...state, basket: updatedDecreaseQty }
+  //
 
+  //     });
+  // }
+  // switch (action.type){
+  //     case "remove from cart":
+  //         const updatedCart = state.basket.filter((item) => (
+  //             item.id !== action.payload
+  //         ));
+  //         console.log("action", action.payload);
 
-        case "cart total price":
-            const totalPrice = state.basket.reduce((initialVal, item) => {
-                // let price = item;
+  //         return {...state, basket:[ updatedCart] };
 
-                initialVal = initialVal + item.price * item.qty;
-                console.log(initialVal);
-                return initialVal;
-            }, 0);
-            return { ...state, total_price: totalPrice }
+  //     default:
+  //         return state;
+  // }
 
-        case "total product":
-
-            // return { ...state, basket: [...state.basket, { ...action.payload }] }
-            // let temp = [1, 2, 3, 4, 5, 6];
-            return { ...state, totalProduct: [...action.payload] }
-
-
-        default:
-            return state;
-    }
-
-    //
-
-    //     });
-    // }
-    // switch (action.type){
-    //     case "remove from cart":
-    //         const updatedCart = state.basket.filter((item) => (
-    //             item.id !== action.payload
-    //         ));
-    //         console.log("action", action.payload);
-
-    //         return {...state, basket:[ updatedCart] };
-
-    //     default:
-    //         return state;
-    // }
-
-    // if(action.type=== "cart total item"){
-    //     return {}
-    // }
-
-
+  // if(action.type=== "cart total item"){
+  //     return {}
+  // }
 };
 // console.log(cartReducer);
 export default cartReducer;
